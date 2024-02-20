7f5273484d449927a160b7375c1258cb7e52e688
Delosreyes <1714124@mukilteo.wednet.edu>
2024-02-20 11:26:15 -0800

    final calculator

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -6,11 +6,358 @@ namespace Vector_Calculator
     {
         static void Main(string[] args)
         {
-            Vector test1 = new Vector(1, 2, 3);
-            Vector test2 = new Vector(-1, 2, -3);
-            
-            // This should be 0, 4, 0
-            Console.WriteLine(Vector.Add(test1, test2));
+            int UserInputX1;
+            int UserInputY1;
+            int UserInputZ1;
+            int UserInputX2;
+            int UserInputY2;
+            int UserInputZ2;
+            int UserInputX3;
+            int UserInputY3;
+            int UserInputZ3;
+            int UserInputX4;
+            int UserInputY4;
+            int UserInputZ4;
+            int UserInputScale;
+
+            string UserNumber;
+
+
+
+            //Add
+
+
+            Console.WriteLine("ADDING");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector1 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ2 = Convert.ToInt32(UserNumber);
+
+            Vector userVector2 = new Vector(UserInputX2, UserInputY2, UserInputZ2);
+
+            Console.WriteLine(Vector.Add(userVector1, userVector2));
+
+
+
+            //Subtract
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine("SUBTRACTING");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector3 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ2 = Convert.ToInt32(UserNumber);
+
+            Vector userVector4 = new Vector(UserInputX2, UserInputY2, UserInputZ2);
+
+            Console.WriteLine(Vector.Subtract(userVector3, userVector4));
+
+
+            // Negate
+
+            Console.WriteLine(" ");
+            Console.WriteLine("NEGATE");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector5 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+            Console.WriteLine(Vector.Negate(userVector5));
+
+
+
+            // Scale
+
+            Console.WriteLine(" ");
+            Console.WriteLine("SCALE");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Scale you'd like to use");
+            UserNumber = Console.ReadLine();
+            UserInputScale = Convert.ToInt32(UserNumber);
+
+            Vector userVector6 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+            Console.WriteLine(Vector.Scale(userVector6, UserInputScale));
+
+
+            // Magnitude
+
+            Console.WriteLine(" ");
+            Console.WriteLine("MAGNITUDE");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector7 = new Vector (UserInputX1, UserInputY1, UserInputZ1);
+
+            Console.WriteLine(userVector7.GetMagnitude());
+
+
+            // Direction
+
+            Console.WriteLine(" ");
+            Console.WriteLine("DIRECTION");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+
+            Vector userVector8 = new Vector(UserInputX1, UserInputY1, 0);
+
+            Console.WriteLine(userVector8.GetDirection());
+
+
+            // Normalize
+
+            Console.WriteLine(" ");
+            Console.WriteLine("NORMALIZE");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+
+            Vector userVector9 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+            Console.WriteLine(Vector.Normalize(userVector9));
+
+            // Dot product
+
+            Console.WriteLine(" ");
+            Console.WriteLine("DOT PRODUCT");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector10 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ2 = Convert.ToInt32(UserNumber);
+
+            Vector userVector11 = new Vector(UserInputX2, UserInputY2, UserInputZ2);
+
+            Console.WriteLine(Vector.DotProduct(userVector10, userVector11));
+
+            // Cross product
+
+            Console.WriteLine(" ");
+            Console.WriteLine("CROSS PRODUCT");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY1 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ1 = Convert.ToInt32(UserNumber);
+
+            Vector userVector12 = new Vector(UserInputX1, UserInputY1, UserInputZ1);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY2 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ2 = Convert.ToInt32(UserNumber);
+
+            Vector userVector13 = new Vector(UserInputX2, UserInputY2, UserInputZ2);
+
+            Console.WriteLine(Vector.CrossProduct(userVector12, userVector13));
+
+            // Angle Between
+
+            Console.WriteLine(" ");
+            Console.WriteLine("ANGLE BETWEEN (Outputs in Rads)");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX3 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY3 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ3 = Convert.ToInt32(UserNumber);
+
+            Vector userVector14 = new Vector(UserInputX3, UserInputY3, UserInputZ3);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX4 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY4 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ4 = Convert.ToInt32(UserNumber);
+
+            Vector userVector15 = new Vector(UserInputX4, UserInputY4, UserInputZ4);
+
+            Console.WriteLine(Vector.AngleBetween(userVector14, userVector15));
+
+            // Projection
+
+            Console.WriteLine(" ");
+            Console.WriteLine("PROJECTION");
+
+            Console.WriteLine("The X of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX3 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY3 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 1st Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ3 = Convert.ToInt32(UserNumber);
+
+            Vector userVector16 = new Vector(UserInputX3, UserInputY3, UserInputZ3);
+
+
+            Console.WriteLine("The X of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputX4 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Y of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputY4 = Convert.ToInt32(UserNumber);
+
+            Console.WriteLine("The Z of the 2nd Vector");
+            UserNumber = Console.ReadLine();
+            UserInputZ4 = Convert.ToInt32(UserNumber);
+
+            Vector userVector17 = new Vector(UserInputX4, UserInputY4, UserInputZ4);
+
+            Console.WriteLine(Vector.ProjectOnto(userVector16, userVector17));
+
         }
     }
+
+    
 }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -7,11 +7,11 @@ namespace Vector_Calculator
         public static readonly Vector Zero = new Vector(1, 1, 1);
         public static readonly Vector One = new Vector(0, 0, 0);
 
-        public float x;
-        public float y;
-        public float z;
+        public double x;
+        public double y;
+        public double z;
 
-        public Vector(float x, float y, float z)
+        public Vector(double x, double y, double z)
         {
             this.x = x;
             this.y = y;
@@ -23,70 +23,60 @@ namespace Vector_Calculator
             return $"<{x}, {y}, {z}>";
         }
 
-        public float GetMagnitude()
+        public double GetMagnitude()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
         }
 
-        public float GetDirection()
+        public double GetDirection()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return Math.Atan(y / x);
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
-        public static Vector Negate(Vector v)
+        public static Vector Negate(Vector v1)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x * -1, v1.y * -1, v1.z * -1);
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
-        public static Vector Scale(Vector v)
+        public static Vector Scale(Vector v1, double scaleNum)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x * scaleNum, v1.y * scaleNum, v1.z * scaleNum);
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v.x / v.GetMagnitude(), v.y / v.GetMagnitude(), v.z / v.GetMagnitude());
         }
 
-        public static float DotProduct(Vector v1, Vector v2)
+        public static double DotProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
         }
 
         public static Vector CrossProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector((v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.z), (v1.x * v2.y) - (v1.y * v2.x));
         }
 
-        public static Vector AngleBetween(Vector v1, Vector v2)
+        public static double AngleBetween(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return Math.Acos(DotProduct(v1, v2) / (v1.GetMagnitude() * v2.GetMagnitude()));
         }
 
         public static Vector ProjectOnto(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            double NewScale = DotProduct(v1, v2) / (Math.Pow(v2.GetMagnitude(), 2));
+            return Vector.Scale(v2, NewScale);
         }
     }
 }
